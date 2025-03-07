e69834bd3e80dc26e2ab0ac6fca5806c998a60ea
Randall Schmidt <randall.schmidt@gala.games>
2025-03-07 09:14:40 -0500

    fix test:sizer

--- test/tutorials/signed-data.test.js
+++ test/tutorials/signed-data.test.js
@@ -47,8 +47,6 @@ describe('signed-data.md', () => {
             [creatorIdentity.address] // constructor-arguments
         );
 
-        console.log(createCode);
-
         // create create-tx
         const rawTx = {
             from: creatorIdentity.address,
